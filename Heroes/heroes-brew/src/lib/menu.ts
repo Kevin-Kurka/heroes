b046147c14b46bf54eac747f2c285bbad5015ae6
Kevin-Kurka <83615166+Kevin-Kurka@users.noreply.github.com>
2026-03-10 21:43:31 -0700

    Update Austin burger description

--- Heroes/heroes-brew/src/lib/menu.ts
+++ Heroes/heroes-brew/src/lib/menu.ts
@@ -147,7 +147,7 @@ export function getMenus(): Menu[] {
             { id: 'b4', name: 'Minneapolis', subtitle: 'Juicy Lucy', description: 'Stuffed cheese patty, grilled onions, cheese skirt, Hero sauce on a brioche bun.', price: 18 },
             { id: 'b5', name: 'Hollywood', subtitle: 'Patty Melt', description: 'White American cheese, grilled onions, Hero sauce on toasted rye bread.', price: 18 },
             { id: 'b6', name: 'Leucadia', subtitle: 'Smash Burger', description: 'Grilled onion, white American cheese, pickles, Hero sauce on a potato bun.', price: 18 },
-            { id: 'b7', name: 'Austin', subtitle: 'Jalapeño Burger', description: 'Pepper jack, wiz, jalapeños, onion ring on a brioche bun.', price: 18 },
+            { id: 'b7', name: 'Austin', subtitle: 'Jalapeño Burger', description: 'Pepper jack, wiz, red pepper relish, grilled onions, jalapeño poppers on a brioche bun.', price: 18 },
             { id: 'b8', name: 'Irwindale', subtitle: 'Sriracha Honey', description: 'Caramelized onions, pickles, slaw, garlic aioli, Sriracha Honey sauce on a brioche bun.', price: 18 },
           ],
         },
